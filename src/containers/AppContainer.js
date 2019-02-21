18fcc335aa7770f2506d71e296680902ad3e8aed
Lebedev Artem <exces-st@yandex.ru>
2019-02-22 00:54:36 +0300

    update container

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,13 +1,15 @@
 import { connect } from 'react-redux';
 import App from '../components/App';
-import { pointsSelector } from '../selectors';
+// import { pointsSelector } from '../selectors';
 import * as actionCreators from '../actions'
 
 
 const mapStateToProps = state => {
   return {
-    points: pointsSelector(state),
+    points: state.points,
     inputText: state.inputText,
+    itemsOrder: state.itemsOrder,
+    yaMapDataRequest: state.yaMapDataRequest,
   }
 }
 
