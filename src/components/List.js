7a94413e7d0b90b19993c7a3ef404a593cfe2e41
Lebedev Artem <exces-st@yandex.ru>
2019-02-22 00:53:18 +0300

    add component List

---
+++ src/components/List.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Item from './Item';
+import './List.css';
+
+
+export default class List extends React.Component {
+  onSortItems = (items) => {
+    this.props.updatePointsOrder({ items });
+  }
+
+  render() {
+    const { items, order } = this.props;
+
+    const listItems = order.map((id, i) => {
+      return (
+        <Item
+          key={id}
+          onSortItems={this.onSortItems}
+          items={order}
+          sortId={i}
+
+          point={items[id]}
+          removePoint={this.props.removePoint}
+        />
+      );
+    });
+
+    return (
+      <ul className='points-block'>
+        {listItems}
+      </ul>
+    )
+  }
+};
