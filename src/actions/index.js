3fbbb9474cac7e5de49c28a625608a4ab9e581b3
Lebedev Artem <exces-st@yandex.ru>
2019-02-23 20:08:34 +0300

    update actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,22 +3,25 @@ import { createAction } from 'redux-actions';
 import { uniqueId } from 'lodash';
 
 
-export const addPoint = createAction('POINT_ADD', pointObj => ({ ...pointObj, id: uniqueId() }));
+export const updateInputText = createAction('INPUT_TEXT_UPDATE');
+
+export const clearGeoObjects = createAction('GEO_OBJECTS_CLEAR');
+
+export const addPoint = createAction('POINT_ADD', ({ point }) => ({ ...point, id: uniqueId() }));
 export const removePoint = createAction('POINT_REMOVE');
 export const updatePointsOrder = createAction('POINTS_ORDER_UPDATE');
-
-export const updateInputText = createAction('INPUT_TEXT_UPDATE');
 
 export const getYaMapDataRequest = createAction('YA_MAP_DATA_GET_REQUEST');
 export const getYaMapDataSuccess = createAction('YA_MAP_DATA_GET_SUCCESS');
 export const getYaMapDataFailure = createAction('YA_MAP_DATA_GET_FAILURE');
+
 export const getYaMapData = ({ query }) => async (dispatch) => {
   dispatch(getYaMapDataRequest())
-  
+
   try {
     const responce = await axios.get(`https://geocode-maps.yandex.ru/1.x/?apikey=a27d9311-b8cd-4e3b-8168-caee70813258&format=json&geocode=${query}`);
-    const geoObjects = responce.data.response.GeoObjectCollection.featureMember
-    geoObjects.map(i => dispatch(getYaMapDataSuccess({ point: i, id: uniqueId() })))
+    const rawGeoObjects = responce.data.response.GeoObjectCollection.featureMember
+    rawGeoObjects.map(rawGeoObject => dispatch(getYaMapDataSuccess({ geoObject: rawGeoObject.GeoObject })))
   } catch(err) {
     dispatch(getYaMapDataSuccess());
   }
