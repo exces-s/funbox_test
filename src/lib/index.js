d38312e24692757af62c5197932066833fc455d6
Lebedev Artem <exces-st@yandex.ru>
2019-02-24 19:02:49 +0300

    add functions in lib

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,4 @@
-import { omit } from 'lodash';
+import { omit, last } from 'lodash';
 
 
 export const createLable = (geoObject) => {
@@ -17,4 +17,24 @@ export const createPointObject = (geoObject) => {
   const rawPoint = omit(geoObject, ['id', 'type'])
 
   return { ...rawPoint, type: 'point' }
+}
+
+export const getCoordsArr = (geoObject) => {
+  const stringCoords = geoObject.Point.pos;
+  const [ lon, lat ] = stringCoords.split(' ');
+  return [lat, lon];
+}
+
+export const getLastPointCoords = (pointsOrder, points) => {
+  const lastOrderPointId = last(pointsOrder)
+
+  if (!lastOrderPointId) {
+    return undefined
+  }
+
+  return getCoordsArr(points[lastOrderPointId])
+}
+
+export const getPolylineCoords = (pointsOrder, points) => {
+  return pointsOrder.map(id => getCoordsArr(points[id]))
 }
