994856c73c5ee036171bb84ec6369185c8ee401f
Lebedev Artem <exces-st@yandex.ru>
2019-02-25 23:07:16 +0300

    add selectors

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,10 +1,30 @@
 import { createSelector } from 'reselect';
+import { createOptionsArr, getLastPointCoords, getPolylineCoords } from '../lib';
 
 
-export const getPoints = state => state.points;
+const getGeoObjects = state => state.geoObjects;
 
-export const pointsSelector = createSelector(
+export const createOptionsArrSelector = createSelector(
+  getGeoObjects,
+  geoObjects => createOptionsArr(geoObjects)
+)
+
+
+const getPoints = state => state.points;
+const getPointsOrder = state => state.pointsOrder;
+
+export const lastPointCoordsSelector = createSelector(
   getPoints,
-  points => Object.values(points),
-);
+  getPointsOrder,
+  (points, pointsOrder) => getLastPointCoords(pointsOrder, points)
+)
+
+export const polylineCoordsSelector = createSelector(
+  getPoints,
+  getPointsOrder,
+  (points, pointsOrder) => getPolylineCoords(pointsOrder, points)
+)
+
+
+
 
