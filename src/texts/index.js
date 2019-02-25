24ff51c6590139e5bc98922300b65733dbcde83f
Lebedev Artem <exces-st@yandex.ru>
2019-02-26 01:58:02 +0300

    separate texts

---
+++ src/texts/index.js
@@ -0,0 +1,17 @@
+export default {
+  form: {
+    placeholder: 'Enter place name',
+  },
+  item: {
+    title: 'Remove point',
+  },
+  actions: {
+    errorAlert: `Sorry, there's some mistake.\nPlease, refresh the page and try again`,
+  },
+  asyncState:  {
+    requested: 'requested',
+    successed: 'successed',
+    failured: 'failured',
+    default: 'none',
+  }
+}
